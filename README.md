f8267b15835ceef205f73d43dbdd3aa7e4517b1a
mikeasta <m.astashonak@gmail.com>
2025-12-03 11:11:32 +0300

    Camera ready. Readme formed

---
+++ README.md
@@ -0,0 +1,16 @@
+# Real-Time Russian Sign Language Recognition
+Computer vision model for russian sign language recognition.
+## Summary
+<br>**Dataset:** https://www.kaggle.com/datasets/kamillakabardieva/russian-sign-language-alphabet
+<br>**Model:** Basic CNN (70k parameters)
+![alt text](images/model.png.png)
+<br>**Results:** 99.8% AUROC, 90% accuracy on test data
+![alt text](images/auroc.png.png)
+<br>**Stack**: Python, PyTorch (Torchvision, Torchmetrics), OpenCV, MLFlow
+
+## Examples
+![alt text](images/A.png)
+![alt text](images/T.png)
+![alt text](images/Л.png)
+![alt text](images/Е.png)
+![alt text](images/Ы.png)
--- camera.py
+++ camera.py
@@ -1,26 +1,41 @@
 import os
-import time
 import torch
 import cv2 as cv
+import torchvision
+import numpy as np
 from PIL import Image
 from pathlib import Path
 from model import RSLRmodel
+from torchvision import transforms
 
 # Initialize model
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = RSLRmodel().to(device)
-model.load_state_dict(torch.load(f="./models/rslr_model_epoch_50.pth"))
+model.load_state_dict(torch.load(f="./best_rslr_model.pth"))
 
 # Setup camera
 cap = cv.VideoCapture(0)
-cap.set(cv.CAP_PROP_FRAME_WIDTH, 500)
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, 500)
+frame_width, frame_height = 640, 480
+cap.set(cv.CAP_PROP_FRAME_WIDTH, frame_width)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, frame_height)
 
 # Classes
 current_dir_path = Path.cwd()
 test_data_path = current_dir_path / "data/test"
 
-classes = os.listdir(test_data_path)
+test_transforms = transforms.Compose([
+    transforms.Resize(size=(64, 64)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225]),
+])
+
+dataset = torchvision.datasets.ImageFolder(
+    root=test_data_path, 
+    transform=test_transforms, 
+    target_transform=None
+)
+
+classes = dataset.classes
 print(classes)
 
 # True classes images
@@ -29,27 +44,53 @@ alphabet_path = current_dir_path / "alphabet"
 for letter in os.listdir(alphabet_path):
     letter_char = letter[0]
     letter_path = alphabet_path / letter
-    alphabet[letter_char] = Image.open(letter_path)
+    alphabet[letter_char] = Image.open(letter_path).resize((frame_width, frame_height))
 
-while True:
+cache_size = 100
+labels_queue = []
+
+# Transforms
+frame_transform = transforms.Compose([
+    transforms.Resize(64),  
+    transforms.Pad(padding=(
+        0, 0, 
+        max(0, 64 - 64), 
+        max(0, 64 - 64)
+    ), fill=255),  
+    transforms.CenterCrop(64),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225]),
+])
+
+while True:    
+    # Get frame
     ret, frame = cap.read()
-    resized_frame = cv.resize(
-        src=frame, 
-        dsize=(64, 64)
-    )
-    tensor_frame = torch.Tensor(resized_frame).permute(2, 0, 1).unsqueeze(dim=0).to(device)
-
-    start_time = time.perf_counter()
-    res = model(tensor_frame).argmax(dim=1)
-    pred_label = classes[res]
-    
     if not ret:
         print("No frame got. Check camera status.")
+        continue 
 
-    cv.imshow("Camera", frame)
-    end_time = time.perf_counter()
-    elapsed_time = end_time - start_time
-    print(f"Execution time: {elapsed_time:.4f} seconds")
+    # Resize frame to prepare model input
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    frame_image = Image.fromarray(frame)
+    features = frame_transform(frame_image).unsqueeze(dim=0)
+
+    # Make prediction
+    pred_indexes = model(features).argmax(dim=1)
+    pred_label = classes[pred_indexes.item()]
+
+    # Averaging result over last "cache_size" predictions
+    labels_queue.append(pred_label)
+    if len(labels_queue) > cache_size:
+        labels_queue.pop(0)
+        pred_label = max(set(labels_queue), key=labels_queue.count) # most frequent label
+
+    pred_sign_image = alphabet[pred_label]
+
+    # Stack frame and current prediction
+    frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+    output = np.hstack((pred_sign_image, frame))
+    
+    cv.imshow("Camera", output)
     if cv.waitKey(1) == ord("s"):
         break
 
